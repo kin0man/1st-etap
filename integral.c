c3dd8c57da3532eb0aadba22203bd796a8a9cbe7
Alexander Ilakin <ilakinsasha2023@gmail.com>
2025-05-05 11:29:30 +0300

    integral

--- integral.c
+++ integral.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 #include "main.h"
+#include <float.h>
 
 double integral(func f, double a, double b, double eps){
     int n = 2;  // Минимальное четное количество интервалов
     double h = (b - a) / n;
     double sum_prev = 0.0;
-    double sum_current = f(a) + f(b);
-    
-    sum_current += 4 * f(a + h);
-    sum_current *= h / 3;
+    double sum_current = DBL_MAX;
     
     do {
+        double x1 = a;
+        double x2 = a;
         sum_prev = sum_current;
-        n *= 2;
         h = (b - a) / n;
-        sum_current = f(a) + f(b);
-        // Суммируем с учетом уже вычисленных точек
-        for (int i = 1; i < n; i += 2)
-            sum_current += 4 * f(a + i * h); // Нечетные точки (коэф. 4)
-        for (int i = 2; i < n - 1; i += 2)
-            sum_current += 2 * f(a + i * h);  // Четные точки (коэф. 2)
-        sum_current *= h / 3;
-    } while (fabs(sum_current - sum_prev) > eps);
+        sum_current = 0;
+        for (int i = 0; i < n; i++){
+            x2 += h;
+            sum_current += (x2 - x1) / 6.0 * (f(x1) + 4.0 * f((x1 + x2) / 2) + f(x2));
+            x1 += h;
+        }
+        n *= 2;
+    } while (fabs(sum_current - sum_prev) / 15 >= eps);
     return sum_current;
 }
